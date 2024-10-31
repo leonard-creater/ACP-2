a170f379debfcae107be78019629a199bbac2c47
leonard-creater <smileme12369@gmail.com>
2024-10-31 10:48:28 +0000

    acp 2

---
+++ 24/10/2024/ACP.py
@@ -0,0 +1,15 @@
+a=input("enter value of a:")
+b=input("enter value of b:")
+c=input("enter value of c:")
+
+print(a)
+print(b)
+print(c)
+
+temp=a
+a=b=c
+b=temp
+c=temp
+print(a)
+print(b)
+print(c)
